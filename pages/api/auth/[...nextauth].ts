8590d94b156c57567a46079c3ec81f8f2fb6d350
tequ <git@tequ.dev>
2025-02-02 02:00:27 +0900

    bump next-auth, typescript

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,11 @@
 import NextAuth from 'next-auth'
 
+declare module "next-auth" {
+  interface User {
+    username: string
+  }
+}
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
