35c72f16865a344d992b08d7bfbc4788bec150d5
tequ <git@tequ.dev>
2026-06-28 21:38:10 +0900

    fix type warning

--- state/transactions.ts
+++ state/transactions.ts
@@ -308,10 +308,11 @@ export const getTxFields = (tt?: string) => {
 
   if (!txFields) return {}
 
-  let _txFields = (Object.keys(txFields) as (keyof typeof txFields)[])
+  let _txFields = (Object.keys(txFields) as (keyof TxFields)[])
     .filter(key => !commonFields.includes(key as any) && !isOptionalFieldName(key))
+    .map(key => normalizeFieldName(key) as keyof TxFields)
     .reduce<TxFields>(
-      (tf, key) => ((tf[normalizeFieldName(key) as keyof TxFields] = (txFields as any)[key]), tf),
+      (tf, key) => (((tf as any)[key] = (txFields as any)[key]), tf),
       {}
     )
   return _txFields
@@ -324,8 +325,9 @@ export const getOptionalTxFields = (tt?: string) => {
 
   return Object.keys(txFields)
     .filter(key => !commonFields.includes(key as any) && isOptionalFieldName(key))
+    .map(key => normalizeFieldName(key) as keyof TxFields)
     .reduce<TxFields>(
-      (tf, key) => ((tf[normalizeFieldName(key) as keyof TxFields] = (txFields as any)[key]), tf),
+      (tf, key) => (((tf as any)[key] = (txFields as any)[key]), tf),
       {}
     )
 }
